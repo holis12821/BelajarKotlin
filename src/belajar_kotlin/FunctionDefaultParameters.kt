af620bd94e002360f896d397b5d1442215d8e204
Nurholis <holis.nur072219@gmail.com>
2021-01-18 21:19:35 +0700

    menambahkan file FunctionDefaultParameters.kt

--- src/belajar_kotlin/FunctionDefaultParameters.kt
+++ src/belajar_kotlin/FunctionDefaultParameters.kt
@@ -7,7 +7,18 @@ kita maka kita menggunakan default argument atau variabel parameter*/
     println("Hello $firstName $lastName")
 }
 
+fun whatsApp(firstName: String, lastName: String? = null){
+    if (lastName == null){
+        println("Hello $firstName")
+    } else{
+        println("Hello $firstName $lastName")
+    }
+}
+
 fun main() {
     hello("Eko") //jadi bisa diisi bisa tdk jika ada sebuah variabel parameter yg sekiranya tidak wajib diisi
     hello("Eko", "Khannedy")
+    whatsApp("Eko", "Kurniawan")
+    whatsApp("Budi")
+    whatsApp("Joko", "Nugroho")
 }
